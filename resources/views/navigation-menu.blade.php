76559ff44f8f0e9e1a5e4bf8a92ffb8bb6a4f432
jamilkasan <jamilkasan123@gmail.com>
2022-08-22 11:44:45 -0300

    Setup

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -162,8 +162,8 @@
                 @endif
 
                 <div>
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="font-medium text-base text-white">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-white">{{ Auth::user()->email }}</div>
                 </div>
             </div>
 
