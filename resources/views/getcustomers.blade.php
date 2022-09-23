3b83bfbebb56fcea9da35725eec334e0bbbbbb88
jamilkasan <jamilkasan123@gmail.com>
2022-09-23 12:07:40 -0300

    Setup

--- resources/views/getcustomers.blade.php
+++ resources/views/getcustomers.blade.php
@@ -1,4 +1,15 @@
-@php($customers = \Illuminate\Support\Facades\DB::connection('epas')->table('QB_Customer')->where('FullName', 'LIKE', '%' . $_REQUEST['search'] . '%')->limit(3)->get())
+@if(Auth::user()->users_type_id == 2)
+    @php
+        $salesRep = \Illuminate\Support\Facades\DB::connection('qb_sales')->table('users_salesRep')->where('user_id', \Illuminate\Support\Facades\Auth::user()->id)->first();
+        $customers = \Illuminate\Support\Facades\DB::connection('epas')->table('QB_Customer')->where('ISActive', 1)->where('Name', 'LIKE', '%' . $_REQUEST['search'] . '%')->where('SalesRepRefListID' , $salesRep->salesRep_ListID)->orderBy('Name', 'ASC')->limit(3)->get();
+    @endphp
+@endif
+@if(Auth::user()->users_type_id == 1 or Auth::user()->users_type_id == 5)
+    @php
+        $customers = \Illuminate\Support\Facades\DB::connection('epas')->table('QB_Customer')->where('ISActive', 1)->where('Name', 'LIKE', '%' . $_REQUEST['search'] . '%')->orderBy('Name', 'ASC')->limit(3)->get();
+    @endphp
+@endif
+{{--@php($customers = \Illuminate\Support\Facades\DB::connection('epas')->table('QB_Customer')->where('FullName', 'LIKE', '%' . $_REQUEST['search'] . '%')->limit(3)->get())--}}
 @foreach($customers as $customer)
     @if(is_array($customer))
         <a href="#" onclick="document.getElementById('customer-search-{{ $_REQUEST['id'] }}').value= '{{ $customer['Name'] }}'; "><br><div class="border-b"><span>{{ $customer['Name'] }}</span></div></a>
