30c6bc291f19e9fa8c143d6c27edeb9efb636360
jamilkasan <jamilkasan123@gmail.com>
2022-09-28 09:36:37 -0300

    Setup

--- app/Jobs/Import_Sales_Order_To_QB.php
+++ app/Jobs/Import_Sales_Order_To_QB.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Mail\OrderNew;
+use App\Models\ImportSoInProcess;
 use App\Models\QbCustomer;
 use App\Models\SalesOrder;
 use Illuminate\Bus\Queueable;
@@ -38,15 +39,34 @@ class Import_Sales_Order_To_QB implements ShouldQueue
      */
     public function handle()
     {
-        $model = SalesOrder::query()->where('id', $this->sales_order_id)->first();
-        $c = QbCustomer::query()->where('ListID', $model->CustomerRefListID)->first();
-        $logo = asset('tti-new_email');
-        $qrLogo = asset('tti-email-qr');
-        $currency = 'SRD';
-        DB::connection('qb_sales')->update( "EXEC [dbo].[sp_insert_sales_order_to_quickbook] @sales_order_id = " . $this->sales_order_id. '; SET NOCOUNT ON;');
-        if ($c->Email != '' or $c->Email != null)
+        if (ImportSoInProcess::query()->count() == 0)
         {
-            Mail::to($c->Email)->send(new OrderNew($this->sales_order_id));
+            $in_process = new ImportSoInProcess();
+            $in_process->in_process = 1;
+            $in_process->save();
+            try
+            {
+                $model = SalesOrder::query()->where('id', $this->sales_order_id)->first();
+                $c = QbCustomer::query()->where('ListID', $model->CustomerRefListID)->first();
+                $logo = asset('tti-new_email');
+                $qrLogo = asset('tti-email-qr');
+                $currency = 'SRD';
+                DB::connection('qb_sales')->update( "EXEC [dbo].[sp_insert_sales_order_to_quickbook] @sales_order_id = " . $this->sales_order_id. '; SET NOCOUNT ON;');
+                if ($c->Email != '' or $c->Email != null)
+                {
+                    Mail::to($c->Email)->send(new OrderNew($this->sales_order_id));
+                }
+            }
+            catch (\Exception $e)
+            {
+                ImportSoInProcess::query()->delete();
+            }
+
+
+        }
+        else
+        {
+            $this->delay(now()->addMinutes(5));
         }
 
 
