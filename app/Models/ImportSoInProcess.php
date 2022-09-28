7475f36721fcc381fb3baf566ea578b3a4395a65
jamilkasan <jamilkasan123@gmail.com>
2022-09-28 09:42:40 -0300

    Setup

---
+++ app/Models/ImportSoInProcess.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ImportSoInProcess extends Model
+{
+    use HasFactory;
+    protected $connection = 'picklist';
+    protected $table = 'import_so_in_process';
+}
