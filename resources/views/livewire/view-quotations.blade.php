c657a34ae7ed9897f7807a4fb571e122edbb231d
jamilkasan <jamilkasan123@gmail.com>
2023-01-03 23:11:56 -0300

    Fixed QB job

--- resources/views/livewire/view-quotations.blade.php
+++ resources/views/livewire/view-quotations.blade.php
@@ -286,7 +286,7 @@
                                                                      .'%':
                                                                      number_format($salesOrderItem->SalesOrderLineRate, 2)
                                                                     }} </td>
-                                                                <td style="padding: 2px;text-align: right;border: 1px solid #ddd;">{{$salesOrderItem->SalesOrderLineAmount}}</td>
+                                                                <td style="padding: 2px;text-align: right;border: 1px solid #ddd;">{{$salesOrderItem->SalesOrderLineAmount}}{{ ($item->SalesTaxCodeRefFullName != 'Non'? 'T':'') }}</td>
                                                             </tr>
                                                             @if($item->SalesTaxCodeRefFullName != 'Non')
                                                                 @php($total = $total + $salesOrderItem->SalesOrderLineAmount)
